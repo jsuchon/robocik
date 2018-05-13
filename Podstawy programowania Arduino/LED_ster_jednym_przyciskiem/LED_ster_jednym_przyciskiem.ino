9d925b2fd5d49bc59f55a4ff288fccece1f19115
Jarosław <suchon.jaroslaw@gmail.com>
2018-05-14 00:54:13 +0200

    Sterowanie cyfrowym wyjściem i wejściem
    Dodano komunikat o stanie na wyjście PC

--- Podstawy programowania Arduino/LED_ster_jednym_przyciskiem/LED_ster_jednym_przyciskiem.ino
+++ Podstawy programowania Arduino/LED_ster_jednym_przyciskiem/LED_ster_jednym_przyciskiem.ino
@@ -3,13 +3,20 @@ void setup() {
   pinMode(3, OUTPUT);
   pinMode(8, INPUT_PULLUP);
   digitalWrite(3, HIGH);
+  Serial.begin(9600);
+  Serial.print("Hello World");
  
 }
 
 void loop() {
   
   if(digitalRead(8) == LOW)
-   x = (x == LOW)? HIGH : LOW;
+   { if(x)
+      Serial.print("\nTeraz gasne");
+     else
+     Serial.print("\nTeraz swiece");    
+    x = (x == LOW)? HIGH : LOW;
+   }
  
  digitalWrite(3, x);
  while(digitalRead(8) == LOW) {}
