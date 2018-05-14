c9035110aca5f945625676ec9809afe179bde4e1
jsuchon <suchon.jaroslaw@gmail.com>
2018-05-14 10:56:23 +0200

    Podstawy wyjścia i wejścia analogowego
    Dodadno reprezentacje za pomocą LED

--- Podstawy programowania Arduino/Woltomierz_z_wykresem_na_PC/Woltomierz_z_wykresem_na_PC.ino
+++ Podstawy programowania Arduino/Woltomierz_z_wykresem_na_PC/Woltomierz_z_wykresem_na_PC.ino
@@ -1,12 +1,16 @@
 int odczyt;
 float napiecie;
+float led;
 void setup() {
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
  odczyt = analogRead(A0);
  napiecie = odczyt * (5.0/1023.0);
 Serial.println(napiecie);
+ led = odczyt * (5.0/255.0);
+ analogWrite(3, led);
  delay(250);
 }
