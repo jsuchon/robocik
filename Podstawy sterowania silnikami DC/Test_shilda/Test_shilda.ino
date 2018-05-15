c993c7c997063b3b713c7e9001499f1729f900d2
Jaroslaw <j.suchon@icloud.com>
2018-05-15 22:29:42 +0200

    Test działania docelowego shilda
    Test podłączenia, zasilania i buzzera

---
+++ Podstawy sterowania silnikami DC/Test_shilda/Test_shilda.ino
@@ -0,0 +1,31 @@
+#define LED 13
+#define BUZZER 10
+#define PRZYCISK 2
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(PRZYCISK, INPUT_PULLUP);
+
+  digitalWrite(LED, HIGH ); // migniecie LED
+  delay(500);
+  digitalWrite(LED, LOW );
+  delay(500);
+  
+
+}
+
+void loop() {
+  if( digitalRead(PRZYCISK) == LOW)
+   {
+      digitalWrite(LED, HIGH );
+      analogWrite(BUZZER, 50);
+      delay(250);
+   }
+   else
+   {
+      digitalWrite(LED, LOW );
+      digitalWrite(BUZZER, LOW); 
+   }
+
+}
